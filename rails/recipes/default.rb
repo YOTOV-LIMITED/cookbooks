42efe82a752e8ce7b669e53c4d3fd318755cb544
Bob Burbach <bob.burbach@gmail.com>
2011-01-23 13:07:54 -0800

    Scope for vagrant

--- rails/recipes/default.rb
+++ rails/recipes/default.rb
@@ -61,23 +61,25 @@ end
 #   mode 0644
 # end
 
-execute "Get private config file amazon.yml" do
-  cwd "#{node[:apache][:web_dir]}/apps/#{node[:apache][:name]}/shared/config"
-  command "#{node[:s3sync][:install_path]}/s3sync/s3cmd.rb get #{node[:ubuntu][:aws_config_path]}:amazon.yml amazon.yml"
-  user "#{node[:capistrano][:deploy_user]}"
-  group "#{node[:capistrano][:deploy_user]}"
-end
-execute "Get private config file api_keys.yml" do
-  cwd "#{node[:apache][:web_dir]}/apps/#{node[:apache][:name]}/shared/config"
-  command "#{node[:s3sync][:install_path]}/s3sync/s3cmd.rb get #{node[:ubuntu][:aws_config_path]}:api_keys.yml api_keys.yml"
-  user "#{node[:capistrano][:deploy_user]}"
-  group "#{node[:capistrano][:deploy_user]}"
-end
-execute "Get private config file cloudkicker_config.rb" do
-  cwd "#{node[:apache][:web_dir]}/apps/#{node[:apache][:name]}/shared/config"
-  command "#{node[:s3sync][:install_path]}/s3sync/s3cmd.rb get #{node[:ubuntu][:aws_config_path]}:cloudkicker_config.rb cloudkicker_config.rb"
-  user "#{node[:capistrano][:deploy_user]}"
-  group "#{node[:capistrano][:deploy_user]}"
+unless node[:chef][:roles].include?('vagrant')
+  execute "Get private config file amazon.yml" do
+    cwd "#{node[:apache][:web_dir]}/apps/#{node[:apache][:name]}/shared/config"
+    command "#{node[:s3sync][:install_path]}/s3sync/s3cmd.rb get #{node[:ubuntu][:aws_config_path]}:amazon.yml amazon.yml"
+    user "#{node[:capistrano][:deploy_user]}"
+    group "#{node[:capistrano][:deploy_user]}"
+  end
+  execute "Get private config file api_keys.yml" do
+    cwd "#{node[:apache][:web_dir]}/apps/#{node[:apache][:name]}/shared/config"
+    command "#{node[:s3sync][:install_path]}/s3sync/s3cmd.rb get #{node[:ubuntu][:aws_config_path]}:api_keys.yml api_keys.yml"
+    user "#{node[:capistrano][:deploy_user]}"
+    group "#{node[:capistrano][:deploy_user]}"
+  end
+  execute "Get private config file cloudkicker_config.rb" do
+    cwd "#{node[:apache][:web_dir]}/apps/#{node[:apache][:name]}/shared/config"
+    command "#{node[:s3sync][:install_path]}/s3sync/s3cmd.rb get #{node[:ubuntu][:aws_config_path]}:cloudkicker_config.rb cloudkicker_config.rb"
+    user "#{node[:capistrano][:deploy_user]}"
+    group "#{node[:capistrano][:deploy_user]}"
+  end
 end
 
 gem_package "bundler" do
