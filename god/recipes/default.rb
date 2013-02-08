7f9fa0b98325fcdb1ef6d6d6d17acfbd74e502fa
Peregrinator <bob.burbach@gmail.com>
2013-02-07 17:01:09 -0800

    Remove old god recipe and start with new one from Opscode. Modify for our needs

--- god/recipes/default.rb
+++ god/recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright 2009, Opscode, Inc.
+# Modified 2013, Critical Juncture, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,11 +18,14 @@
 # limitations under the License.
 #
 
-include_recipe "ruby"
-include_recipe "runit"
-
 gem_package "god" do
   action :install
+end
+
+template "/etc/init.d/god" do
+  source "god.initd.conf.erb"
+  mode 0744
+  action :create
 end
 
 directory "/etc/god/conf.d" do
@@ -31,11 +35,34 @@ directory "/etc/god/conf.d" do
   mode 0755
 end
 
-template "/etc/god/master.god" do
+service "god" do
+  supports :status => true, :restart => true
+  action :nothing
+end
+
+
+template "/etc/god/god.conf" do
   source "master.god.erb"
   owner "root"
   group "root"
   mode 0755
+  variables(
+    :domain => node[:god][:domain],
+    :email_name => node[:god][:email_name],
+    :email_domain => node[:god][:email_domain]
+  )
+  notifies :restart, resources(:service => "god")
 end
 
-runit_service "god"
+node[:god][:monitor].each do |monitor|
+  template "/etc/god/conf.d/#{monitor['options']['queue']}.god" do
+    source "#{monitor['name']}.god.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables( 
+      :options => monitor['options']
+    )
+    notifies :restart, resources(:service => "god")
+  end
+end
