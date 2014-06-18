b2ebb004756923e5da9213bfa004d9aa63c95d1f
Peregrinator <bob.burbach@gmail.com>
2014-06-18 12:44:52 -0700

    Ensure my fr logs rotate

--- rails/recipes/logrotate.rb
+++ rails/recipes/logrotate.rb
@@ -1,7 +1,18 @@
 # create a logrotate conf file for the app
-template "/etc/logrotate.d/rails" do
+template "/etc/logrotate.d/#{node[:app][:name]}" do
   source "logrotate.erb"
   variables :log_path  => "#{node[:app][:web_dir]}/apps/#{node[:app][:name]}/current/log",
+            :interval  => node[:rails][:logrotate][:interval],
+            :keep_for  => node[:rails][:logrotate][:keep_for]
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+# temporary until this is refactored in new chef system
+template "/etc/logrotate.d/my_fr2" do
+  source "logrotate.erb"
+  variables :log_path  => "#{node[:app][:web_dir]}/apps/my_fr2/log",
             :interval  => node[:rails][:logrotate][:interval],
             :keep_for  => node[:rails][:logrotate][:keep_for]
   mode 0644
